a165932b8f1871819d764450a0abe684a7a25d41
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-09-04 10:33:46 +0200

    Add  package ref

--- R/normalise.R
+++ R/normalise.R
@@ -54,5 +54,5 @@ standardise <-
 #' @export
 positional_normalisation <-
   function(x) {
-    ((x - median(x)) / sqrt(sum((x - median(x))^2)))
+    ((x - stats::median(x)) / sqrt(sum((x - median(x))^2)))
   }
